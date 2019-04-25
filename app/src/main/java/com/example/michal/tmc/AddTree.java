cc3415a1025541d803d2593e8391665a2c3236ba
Hedrick <djkacpl@gmail.com>
2019-04-25 13:57:41 +0200

    toolbar teksty

--- app/src/main/java/com/example/michal/tmc/AddTree.java
+++ app/src/main/java/com/example/michal/tmc/AddTree.java
@@ -1,7 +1,11 @@
 package com.example.michal.tmc;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class AddTree extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class AddTree extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_tree);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Dodaj drzewo...");
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
+
 }
--- app/src/main/java/com/example/michal/tmc/TreeList.java
+++ app/src/main/java/com/example/michal/tmc/TreeList.java
@@ -1,5 +1,6 @@
 package com.example.michal.tmc;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -14,7 +15,7 @@ public class TreeList extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         Bundle bundle = getIntent().getExtras();
-        String message = bundle.getString("message");
+        final String message = bundle.getString("message");
 
         setContentView(R.layout.activity_tree_list);
         Toolbar toolbar = findViewById(R.id.toolbar);
@@ -27,8 +28,11 @@ public class TreeList extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                //        .setAction("Action", null).show();
+                Intent intent = new Intent(TreeList.this, AddTree.class);
+                intent.putExtra("message", message);
+                startActivity(intent);
             }
         });
     }
